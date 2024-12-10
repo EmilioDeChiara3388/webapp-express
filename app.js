7afd324bbb927aef5e6e7fe63663603344052c62
EmilioDeChiara3388 <emilio.dechiara3388@gmail.com>
2024-12-10 14:10:58 +0100

    add: request get for index movies

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ const connection = require("./database/connection")
 const HOST = process.env.HOST
 const PORT = process.env.PORT
 
+server.get("/", (req, res) => {
+    connection.query(`SELECT * FROM movies`, (err, results) => {
+        if (err) return res.status(500).json({ err: err })
+        res.json({
+            movies: results
+        })
+    })
+})
 
 server.listen(PORT, () => {
     console.log(`Server is listening on port ${HOST}:${PORT}`);
