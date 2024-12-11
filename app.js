e285646234eff236dcfe72e7baea08e02014d88e
EmilioDeChiara3388 <emilio.dechiara3388@gmail.com>
2024-12-11 17:45:26 +0100

    add: cors

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const server = express();
-const movieRouter = require("./routes/movies.js")
+const movieRouter = require("./routes/movies.js");
+const cors = require('cors');
 
 const HOST = process.env.HOST
 const PORT = process.env.PORT
 
 server.use("/movies", movieRouter)
+
+server.use(cors())
 
 server.listen(PORT, () => {
     console.log(`Server is listening on port ${HOST}:${PORT}`);
--- routes/movies.js
+++ routes/movies.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const moviesController = require("../controllers/moviesController.js")
+const moviesController = require("../controllers/moviesController.js");
 
 router.get("/", moviesController.index)
 
